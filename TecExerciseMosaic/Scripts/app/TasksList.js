6787e71cad17b40057e0248db06ae1d45955224d
Isaac Palma <aelzhas@hotmail.com>
2022-12-01 14:59:37 +0000

    updated

---
+++ TecExerciseMosaic/Scripts/app/TasksList.js
@@ -0,0 +1,29 @@
+﻿function tachar(id) {
+    var taskId = id;
+    var link = markTasksUrl;
+
+    link = link.replace("taskId", taskId);
+
+    $.ajax({
+        url: link, success: function (result) {
+            location.reload();
+        }
+    });
+}
+
+function createTask() {
+    var link = addTasksUrl;
+
+    var data = $('#txtNewTask').val();
+
+    if (data === undefined || data === "")
+        return;
+    link = link.replace("description", data);
+
+    $.ajax({
+        url: link, success: function (result) {
+            location.reload();
+        }
+    });
+
+}
